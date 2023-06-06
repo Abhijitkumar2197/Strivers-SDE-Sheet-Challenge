39d1cb64b55958e8edb9b5e29073140981acccb6
MIster_Mikasa <78596061+Abhijitkumar2197@users.noreply.github.com>
2023-06-07 00:03:06 +0530

    Create KthSmallestAndLargestElement.java

---
+++ KthSmallestAndLargestElement.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+
+public class Solution {
+	public static ArrayList<Integer> kthSmallLarge(ArrayList<Integer> arr, int n, int k) {
+		Collections.sort(arr);
+		ArrayList<Integer> ans = new ArrayList<>();
+		ans.add(arr.get(k - 1));
+		ans.add(arr.get(arr.size() - k));
+		return ans;
+	}
+}
