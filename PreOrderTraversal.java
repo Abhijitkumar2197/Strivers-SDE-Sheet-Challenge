26c18ca8558414d3d11fe912a9b33c422d668d8b
MIster_Mikasa <78596061+Abhijitkumar2197@users.noreply.github.com>
2023-07-03 09:20:26 +0530

    Create PreOrderTraversal.java

---
+++ PreOrderTraversal.java
@@ -0,0 +1,37 @@
+import java.util.* ;
+import java.io.*; 
+/*
+	
+	Following is the Binary Tree node structure:
+
+	public class TreeNode {
+	    int data;
+	    TreeNode left;
+	    TreeNode right;
+	    TreeNode() {}
+	    TreeNode(int val) { this.data = val; }
+	    TreeNode(int val, TreeNode left, TreeNode right) {
+	       this.data = val;
+	        this.left = left;
+	        this.right = right;
+	    }
+	}
+
+*/
+
+public class Solution {
+    public static List < Integer > getPreOrderTraversal(TreeNode root) {
+    	// Write your code here.
+		List<Integer> ans = new ArrayList();
+        helper(root, ans);
+        return ans;
+    }
+	public static void helper(TreeNode root, List < Integer > ans){
+        if(root == null) return;
+		ans.add(root.data);
+        helper(root.left,ans);
+        
+        helper(root.right, ans);
+
+    }
+}
