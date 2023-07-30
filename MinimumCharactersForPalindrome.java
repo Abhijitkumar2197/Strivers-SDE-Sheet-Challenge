a74a30efb758d6e8d1083f0d956c450ba3588e9d
MIster_Mikasa <78596061+Abhijitkumar2197@users.noreply.github.com>
2023-07-30 15:41:18 +0530

    Create MinimumCharactersForPalindrome.java

---
+++ MinimumCharactersForPalindrome.java
@@ -0,0 +1,19 @@
+
+public class Solution {
+
+    public static int minCharsforPalindrome(String str) {
+        int i=0, j=str.length()-1,chekced = str.length() - 1, res=0;
+        while(i<j){
+            if(str.charAt(i)==str.charAt(j)){
+                i++;
+                j--;
+            }else{
+                res++;
+                i = 0;
+                j = --chekced;
+            }
+        }
+        return res;
+    }
+
+}
