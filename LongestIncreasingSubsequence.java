9730fb32f6f079cc8af8193263161aa85fd25b09
MIster_Mikasa <78596061+Abhijitkumar2197@users.noreply.github.com>
2023-07-30 17:06:26 +0530

    Update LongestIncreasingSubsequence.java

--- LongestIncreasingSubsequence.java
+++ LongestIncreasingSubsequence.java
@@ -1,22 +1,27 @@
 import java.util.*;
 public class Solution {
 
-	public static int longestIncreasingSubsequence(int arr[]) {
-		int[] dp = new int[arr.length];
-        int n = arr.length;
-        Arrays.fill(dp, 1);
-        for(int i = n - 1; i >= 0; i--){
-            for( int j  = i; j >= 0 ; j--){
-                if(arr[j] < arr[i] ){
-                    dp[j] = Math.max(dp[j] , dp[i] + 1);
+	public static int longestIncreasingSubsequence(int nums[]) {
+		int n = nums.length;
+        List<Integer> lis = new ArrayList<Integer>();
+        for(int i=0;i<n;i++){
+            if(lis.isEmpty() || nums[i] > lis.get(lis.size()-1)){
+                lis.add(nums[i]);
+            }
+            else{
+                int possiblePos = Collections.binarySearch(lis, nums[i]);
+                if(possiblePos >=0){
+                    continue;
+                }
+                else{
+                    int insertPos = -possiblePos -1;
+                    lis.set(insertPos,nums[i]);
                 }
             }
+
         }
-        int max = 1;
-        for(int i = 0; i < arr.length; i++){
-            max = Math.max(max, dp[i]);
-        }
-        return max;
+        
+        return lis.size();
 	}
 
 }
